adc71db9c74af674def8921321d9fb15b69c86c8
buxuesong <buxuesong@foxmail.com>
2022-06-10 22:30:07 +0800

    普通先进先出队列

---
+++ src/main/java/com/xuesong/datastructure/queue/NormalQueue.java
@@ -0,0 +1,81 @@
+package com.xuesong.datastructure.queue;
+
+/**
+ * 先进先出普通队列
+ * @param <E>
+ */
+public class NormalQueue<E> {
+    private SinglyLinkedNode first;
+    private SinglyLinkedNode last;
+
+    /**
+     * 通过单链表即可实现
+     * @param <E>
+     */
+    public static class SinglyLinkedNode <E> {
+        public SinglyLinkedNode (E value) {
+            this.value = value;
+        }
+        private E value;
+        private SinglyLinkedNode next;
+
+        public E getValue() {
+            return value;
+        }
+
+        public void setValue(E value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "SinglyLinkedNode{" +
+                    "value=" + value +
+                    '}';
+        }
+    }
+
+    /**
+     * 从队尾插入新节点
+     * @param e
+     */
+    public void push(E e){
+        SinglyLinkedNode newNode = new SinglyLinkedNode(e);
+        if (first == null) {
+            first = newNode;
+        }
+        if (last == null) {
+            last = newNode;
+        } else {
+            last.next = newNode;
+            last = newNode;
+        }
+    }
+    /**
+     * 从队首取出节点
+     */
+    public E pop(){
+        if (first == null) {
+            return null;
+        }
+        SinglyLinkedNode<E> node = first;
+        first = first.next;
+        if(first == null){
+            last = null;
+        }
+        return node.getValue();
+    }
+
+    public static void main(String[] args) {
+        NormalQueue<String> queue = new NormalQueue<String>();
+        queue.push("a");
+        queue.push("b");
+        queue.push("c");
+        queue.push("d");
+        String result = queue.pop();
+        while(result!=null){
+            System.out.println("pop == "+result);
+            result = queue.pop();
+        }
+    }
+}
