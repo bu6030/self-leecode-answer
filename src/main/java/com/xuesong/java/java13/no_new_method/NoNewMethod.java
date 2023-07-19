37253b0a9eae9810c621efbd25be4a6231c14fba
Xuesong.Bu <Xuesong.Bu@homecredit.cn>
2023-07-19 11:28:25 +0800

    增加 JAVA19 新方法 都是预览或孵化，没有可用的新代码

--- src/main/java/com/xuesong/java/java13/no_new_method/NoNewMethod.java
+++ src/main/java/com/xuesong/java/java13/no_new_method/NoNewMethod.java
@@ -1,4 +1,7 @@
 package com.xuesong.java.java13.no_new_method;
 
+/**
+ * JAVA 13 过度版本，没有什么新增的，都是预览或孵化
+ */
 public class NoNewMethod {
 }
---
+++ src/main/java/com/xuesong/java/java19/no_new_method/NoNewMethod.java
@@ -0,0 +1,7 @@
+package com.xuesong.java.java19.no_new_method;
+
+/**
+ * JAVA 19 过度版本，没有什么新增的，都是预览或孵化
+ */
+public class NoNewMethod {
+}
